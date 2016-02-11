795d74887718c7f3d936f1a9c73b66707b7b5c8d
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-11 01:10:34 -0500

    Initial commit

--- app/src/main/java/com/jdm5908_bw/ist402/whack_a_mole/MainActivity.java
+++ app/src/main/java/com/jdm5908_bw/ist402/whack_a_mole/MainActivity.java
@@ -2,12 +2,35 @@ package com.jdm5908_bw.ist402.whack_a_mole;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    // UI References
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeReferences();
+
     }
+
+    /**
+     * Creates the references to the buttons.
+     */
+   private void initializeReferences(){
+
+       button1 = (Button) findViewById(R.id.button1);
+       button2 = (Button) findViewById(R.id.button2);
+       button3 = (Button) findViewById(R.id.button3);
+       button4 = (Button) findViewById(R.id.button4);
+       button5 = (Button) findViewById(R.id.button5);
+       button6 = (Button) findViewById(R.id.button6);
+       button7 = (Button) findViewById(R.id.button7);
+       button8 = (Button) findViewById(R.id.button8);
+       button9 = (Button) findViewById(R.id.button9);
+
+   }
 }
